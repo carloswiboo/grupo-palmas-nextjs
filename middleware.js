9d102b7a4bf31df00b55e2a922a860367d7e2f8e
Carlos Francisco Estrada Garcidueñas <carlos@wiboo.com.mx>
2024-05-19 20:22:50 -0600

    Ajustando Middleware

--- middleware.js
+++ middleware.js
@@ -1,7 +1,11 @@
 import { NextResponse } from "next/server";
+import { verifyAuth } from "./lib/auth";
 
 // This function can be marked `async` if using `await` inside
-export function middleware(request) {
+export async function middleware(request) {
+  let verifiedToken = await verifyAuth(request);
+
+  debugger;
   return NextResponse.next();
 }
 
