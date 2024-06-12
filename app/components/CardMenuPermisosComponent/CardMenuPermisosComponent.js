be12f0274798f7ea6945184e1af48b73cc40c4cc
Carlos Francisco Estrada Garcidueñas <carlos@wiboo.com.mx>
2024-06-12 14:17:32 -0600

    Actualizando paquetes

---
+++ app/components/CardMenuPermisosComponent/CardMenuPermisosComponent.js
@@ -0,0 +1,27 @@
+"use client";
+import React from "react";
+import {
+  ActionList,
+  BlockStack,
+  Button,
+  ButtonGroup,
+  Card,
+  InlineGrid,
+  List,
+  Popover,
+  Text,
+} from "@shopify/polaris";
+const CardMenuPermisosComponent = (props) => {
+  return (
+    <>
+      {" "}
+      <Card key={index}>
+        <Text as="h2" variant="bodyMd">
+          Content inside a card
+        </Text>
+      </Card>
+    </>
+  );
+};
+
+export default CardMenuPermisosComponent;
--- app/dashboard/menuyfunciones/page.js
+++ app/dashboard/menuyfunciones/page.js
@@ -1,7 +1,29 @@
+"use client";
 import React from "react";
+import { getMenuYfunciones } from "../../../lib/api/apiMenuYFunciones";
 
-const page = () => {
-  return <div>page</div>;
+const MenuYFuncionesScreenComponent = () => {
+  const [finalData, setFinalData] = React.useState([]);
+
+  React.useEffect(() => {
+    getMenuYfunciones().then((response) => {
+      if (response.status === 200) {
+        setFinalData(response.data);
+      } else {
+        setFinalData([]);
+      }
+    });
+  }, []);
+
+  return (
+    <>
+      <div className="grid grid-cols-1 gap-4">
+        {finalData.map((item, index) => (
+          <CardMenuPermisosComponent key={index} />
+        ))}
+      </div>
+    </>
+  );
 };
 
-export default page;
+export default MenuYFuncionesScreenComponent;
